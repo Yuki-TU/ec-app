f6fea4ee0097dbe4658a91e21bd0158405aa53c6
Yuki-TU <k.yuki.90219@icloud.com>
2022-01-07 00:30:06 +0900

    add:#5 eslint設定ファイルの設定 eslintのルールはairbnbで対応

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "airbnb",
+    "airbnb-typescript", // airbnb, airbnb-typescriptの順番で記述
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "eslint-config-prettier", // prettirとeslintの競合を避ける,最後に入れること
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+    project: "./tsconfig.json", // airbnb-typescriptで利用する
+  },
+  settings: {
+    react: {
+      version: "detect", // reactのバージョンを設定,eslint-plugin-reactで利用
+    },
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
+};
