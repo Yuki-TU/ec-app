230ec95dc2dc27b4003caba24f71284cf66d8e0f
Yuki-TU <k.yuki.90219@icloud.com>
2022-01-12 19:30:56 +0900

    add:#9 汎用ユーザー入力コンポーネント及びテストコード

---
+++ src/components/uiParts/TextInput/presenter.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { useStyles } from './style';
+
+/** TextInputの引数の型定義 */
+type TextInputProps = {
+  /** フルサイズ指定(true: フルサイズ, false: フルサイズではない) */
+  fullWidth: boolean;
+  /** テキスト入力欄のラベル */
+  label: string;
+  /** 複数業表示(true: 有効, false: 無効) */
+  multiline: boolean;
+  /** 必須入力かどうか */
+  required: boolean;
+  /** 複数業表示数場合の行数 */
+  rows: string | number;
+  /** 入力値(初期表示などに利用) */
+  value: string;
+  /** 入力タイプ(password, email, text) */
+  type: string;
+  /** 値を入力した時に実行されるコールバック関数 */
+  onChange: React.ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>;
+};
+
+/**
+ * テキスト入力の汎用コンポーネント
+ * @param props - 各種設定値
+ * @return コンポーネント
+ */
+function TextInput(props: TextInputProps) {
+  const classes = useStyles();
+  const { fullWidth, label, multiline, required, rows, value, type, onChange } =
+    props;
+  const textStyle = fullWidth ? classes.full : classes.half;
+
+  return (
+    <TextField
+      className={textStyle}
+      fullWidth={fullWidth}
+      label={label}
+      margin="dense"
+      multiline={multiline}
+      required={required}
+      rows={rows}
+      value={value}
+      type={type}
+      onChange={onChange}
+    />
+  );
+}
+
+export default TextInput;
