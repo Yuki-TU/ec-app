43bcf44a2a26889ee08b242ac5fb63b6d03ddbb5
Yuki-TU <k.yuki.90219@icloud.com>
2022-01-12 19:30:04 +0900

    add:#9 汎用ボタンのコンポーネント及びテストコード

---
+++ src/components/uiParts/PrimaryButton/presenter.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { PrimaryButton } from '.';
+import { useStyles } from './style';
+
+// スタイル指定のuseStyles関数はメソッド化
+jest.mock('./style');
+const mockUseStyles = useStyles as jest.Mock;
+
+describe('PrimeButtonンポーネントは汎用ボタンを表示', () => {
+  beforeEach(() => {
+    // useStyleの返り値を空のオブジェクトに設定
+    mockUseStyles.mockReturnValue({});
+    render(<PrimaryButton label="ok" onClick={() => {}} />);
+  });
+  test('ボタンタグを表示している', () => {
+    expect(screen.getByRole('button')).toBeEnabled();
+  });
+  test('labelに指定した文字を表示している', () => {
+    expect(screen.queryByText('ok')).toBeInTheDocument();
+  });
+});
---
+++ src/components/uiParts/PrimaryButton/style.ts
@@ -0,0 +1,21 @@
+import { makeStyles } from '@material-ui/styles';
+import { createStyles, Theme } from '@material-ui/core';
+
+/**
+ * スタイル定義
+ */
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    button: {
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.primary.contrastText,
+      fontSize: 16,
+      height: 48,
+      marginBottom: 16,
+      width: 256,
+      '&:hover': {
+        backgroundColor: theme.palette.primary.dark,
+      },
+    },
+  })
+);
