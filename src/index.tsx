2395c9df829b70096c2e2e94e97ba7e7601f5339
Yuki-TU <k.yuki.90219@icloud.com>
2022-01-12 19:43:38 +0900

    add:#9 コンポーネント内でhistoryを扱えるように設定

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as History from 'history';
+import { ConnectedRouter } from 'connected-react-router';
 import App from './App';
+/** URIの履歴を保持するhistory変数 */
+const history = History.createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ConnectedRouter history={history}>
+          <App />
+      </ConnectedRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
