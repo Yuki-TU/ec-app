0f1b26081a3f908f2de169afd67673b3957cb7b8
Yuki-TU <k.yuki.90219@icloud.com>
2022-01-12 19:57:38 +0900

    add:#9 appコンポーネントにスタイル適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import Router from './Router';
 
+/** スタイル */
+const useStyles = makeStyles({
+  root: {
+    fontSize: '62.5%',
+  },
+});
+
+/**
+ * ページ全体の関数コンポーネント
+ * @return ページ全体のコンポーネント
+ */
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
+    <div className={classes.root}>
+      <main>
+        <Router />
+      </main>
     </div>
   );
 }
