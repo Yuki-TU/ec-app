7c7c89b21e0bf7b2a38a3dd0164d5b1257ed7ed2
Yuki-TU <k.yuki.90219@icloud.com>
2022-01-17 01:07:46 +0900

    add:#19 サインイン状況に応じて処理するAuthコンポーネントの定義と利用 サインインしていない時Authコンポーネントの子要素は表示しないように制御する サインインの時、トップページではメールアドレスとユーザ名表示

---
+++ src/Auth.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, ReactElement } from 'react';
+import { useDispatch } from 'react-redux';
+import { loadSignedIn } from './reducks/users/selectors';
+import { listenAuthState } from './reducks/users/operation';
+import { useSelector } from './reducks/store';
+
+/**
+ * サインインであれば子要素を表示し、そうでなければ子要素は表示しない
+ * @param param0 children - 子要素
+ * @returns サインイン状況に応じたコンポーネント
+ */
+function Auth({ children }: { children: ReactElement }) {
+  const dispatch = useDispatch();
+
+  const selector = useSelector((state) => state);
+  const isSignedIn = loadSignedIn(selector);
+
+  useEffect(() => {
+    if (!isSignedIn) {
+      // 初回表示にログイン状態でないければ、firebase内でログイン状態を確認
+      dispatch(listenAuthState());
+    }
+  }, []);
+
+  // サインインしていないければ、Authの子要素は表示させない
+  if (!isSignedIn) {
+    return <div />;
+  }
+  // サインインしていれば、Authの子要素は表示させる
+  return children;
+}
+
+export default Auth;
