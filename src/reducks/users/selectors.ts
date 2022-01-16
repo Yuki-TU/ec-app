c61425bd44a7574a23c27428be5d70d166957830
Yuki-TU <k.yuki.90219@icloud.com>
2022-01-16 16:44:05 +0900

    add:#16 ストアの値を取得するselectorsの定義 メールアドレスとユーザ名を取得

---
+++ src/reducks/users/selectors.ts
@@ -0,0 +1,15 @@
+import { createSelector } from 'reselect';
+import { RootState } from '../store';
+
+const userSelector = (state: RootState) => state.user;
+
+/** ユーザメールアドレスの取得 */
+export const loadUserEmail = createSelector(
+  [userSelector],
+  (state) => state.email
+);
+/** ユーザー名の取得 */
+export const loadUserName = createSelector(
+  [userSelector],
+  (state) => state.username
+);
