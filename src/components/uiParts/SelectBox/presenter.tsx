37c925b72599e7b63c2e2d1642ace37ada8a79bc
Yuki-TU <k.yuki.90219@icloud.com>
2022-01-21 02:06:29 +0900

    add:#27 選択コンポーネントを策英

---
+++ src/components/uiParts/SelectBox/presenter.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import { useStyles } from './style';
+
+type SelectBoxProps = {
+  /** 表示ラベル */
+  label: string;
+  /** 選択されたアイテム */
+  selectedItem: string;
+  /** 必須項目か */
+  required: boolean;
+  /** アイテムが偏向された時に走るコールバック関数 */
+  onChange: (
+    event: React.ChangeEvent<{
+      name?: string | undefined;
+      value: unknown;
+    }>,
+    child: React.ReactNode
+  ) => void;
+  /** 選択できるアイテムたち */
+  items: { id: string; name: string }[];
+};
+
+/**
+ * 選択ボックスのコンポーネント
+ * @param props - {label: 表示ラベル, selectedItem: 選択のアイテム, reqired: 必須か, onChange: コールバック, items: アイテム}
+ * @returns コンポーネント
+ */
+function SelectBox(props: SelectBoxProps) {
+  const classes = useStyles();
+  const { label, selectedItem, required, onChange, items } = props;
+
+  return (
+    <FormControl className={classes.formControl}>
+      <InputLabel>{label}</InputLabel>
+      <Select value={selectedItem} required={required} onChange={onChange}>
+        {items.map((item) => (
+          <MenuItem key={item.id} value={item.id}>
+            {item.name}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+}
+
+export default SelectBox;
---
+++ src/components/uiParts/SelectBox/style.ts
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles(() => ({
+  formControl: {
+    marginBottom: 16,
+    minWidth: 120,
+    width: '100%',
+  },
+}));
