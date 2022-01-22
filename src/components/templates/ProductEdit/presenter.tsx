8171821918065cd04ff66b0c5900eacee41de4f2
Yuki-TU <k.yuki.90219@icloud.com>
2022-01-22 19:05:33 +0900

    add:#27 商品編集画面のコンポーネント作成

---
+++ src/components/templates/ProductEdit/presenter.tsx
@@ -0,0 +1,140 @@
+import React, { useCallback, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { saveProduct } from '../../../reducks/products/operations';
+import { PrimaryButton } from '../../uiParts/PrimaryButton';
+import { SelectBox } from '../../uiParts/SelectBox';
+import { TextInput } from '../../uiParts/TextInput';
+import { useStyles } from './style';
+
+/**
+ * 商品編集をする画面の古音ポーネンと
+ * @returns コンポーネント
+ */
+function ProductEdit() {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  // 商品情報に必要なステートの定義
+  const [productName, setProductName] = useState('');
+  const [productDescription, setProductDiscription] = useState('');
+  const [category, setCategory] = useState('');
+  const [gender, setGender] = useState('');
+  const [productPrice, setProductPrice] = useState('');
+
+  // プルダウン選択に必要なデータ定義
+  const categories = [
+    { id: 'game', name: 'ゲーム' },
+    { id: 'shoes', name: 'シューズ' },
+    { id: 'clothes', name: '洋服' },
+    { id: 'pc', name: 'パソコン' },
+    { id: 'book', name: '書籍' },
+    { id: 'interior', name: 'インテリア' },
+    { id: 'cosme', name: 'コスメ' },
+    { id: 'vehicle', name: '乗り物' },
+    { id: 'hobby', name: 'ホビー' },
+  ];
+  const genders = [
+    { id: 'male', name: 'メンズ' },
+    { id: 'female', name: 'レディース' },
+    { id: 'unisex', name: 'ユニセックス' },
+  ];
+
+  const inputProductName = useCallback(
+    (e) => {
+      setProductName(e.target.value);
+    },
+    [setProductName]
+  );
+  const inputProductDescription = useCallback(
+    (e) => {
+      setProductDiscription(e.target.value);
+    },
+    [setProductDiscription]
+  );
+  const inputProductPrice = useCallback(
+    (e) => {
+      setProductPrice(e.target.value);
+    },
+    [setProductPrice]
+  );
+  const inputCategory = useCallback(
+    (e) => {
+      setCategory(e.target.value);
+    },
+    [setCategory]
+  );
+  const inputGender = useCallback(
+    (e) => {
+      setGender(e.target.value);
+    },
+    [setGender]
+  );
+
+  return (
+    <div className={classes.root}>
+      <h2 className={classes.title}>商品の登録</h2>
+      <TextInput
+        fullWidth
+        label="商品名"
+        multiline={false}
+        rows={1}
+        required
+        value={productName}
+        type="text"
+        onChange={inputProductName}
+      />
+      <TextInput
+        fullWidth
+        label="商品説明"
+        multiline
+        rows={5}
+        required
+        value={productDescription}
+        type="text"
+        onChange={inputProductDescription}
+      />
+      <SelectBox
+        label="カテゴリ"
+        required
+        items={categories}
+        onChange={inputCategory}
+        selectedItem={category}
+      />
+      <SelectBox
+        label="性別"
+        required
+        items={genders}
+        selectedItem={gender}
+        onChange={inputGender}
+      />
+      <TextInput
+        fullWidth
+        label="値段(円)"
+        multiline={false}
+        rows={1}
+        required
+        value={productPrice}
+        type="number"
+        onChange={inputProductPrice}
+      />
+      <div className={classes.button}>
+        <PrimaryButton
+          label="保存"
+          onClick={() =>
+            dispatch(
+              saveProduct(
+                productName,
+                productDescription,
+                category,
+                gender,
+                productPrice
+              )
+            )
+          }
+        />
+      </div>
+    </div>
+  );
+}
+
+export default ProductEdit;
---
+++ src/components/templates/ProductEdit/style.ts
@@ -0,0 +1,21 @@
+import { makeStyles } from '@material-ui/styles';
+
+/** スタイル設定 */
+export const useStyles = makeStyles({
+  root: {
+    margin: '0 auto',
+    maxWidth: '400px',
+    padding: '1rem',
+    height: 'auto',
+    width: 'calc(100% - 2rem)',
+  },
+  title: {
+    textAlign: 'center',
+    color: '#4dd0e1',
+    fontSize: '1.563rem',
+    margin: '0 auto 1rem auto',
+  },
+  button: {
+    marginTop: '5rem',
+  },
+});
