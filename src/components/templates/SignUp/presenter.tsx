1b2a572b00d943a3c80de913abc094410efe3c86
Yuki-TU <k.yuki.90219@icloud.com>
2022-01-29 16:28:12 +0900

    add:#33 サインアップコンポーネントをform検証対応

--- src/components/templates/SignUp/presenter.tsx
+++ src/components/templates/SignUp/presenter.tsx
@@ -71,54 +71,58 @@ function SignUp() {
   return (
     <div className={classes.root}>
       <h2 className={classes.title}>アカウント登録</h2>
-      <TextInput
-        fullWidth
-        label="ユーザ名"
-        multiline={false}
-        required
-        rows={1}
-        value={userName}
-        type="text"
-        onChange={inputUserName}
-      />
-      <TextInput
-        fullWidth
-        label="メールアドレス"
-        multiline={false}
-        required
-        rows={1}
-        value={email}
-        type="email"
-        onChange={inputEmail}
-      />
-      <TextInput
-        fullWidth
-        label="パスワード（半角英数字6文字以上）"
-        multiline={false}
-        required
-        rows={1}
-        value={password}
-        type="password"
-        onChange={inputPassword}
-      />
-      <TextInput
-        fullWidth
-        label="パスワードの再確認"
-        multiline={false}
-        required
-        rows={1}
-        value={confirmPassword}
-        type="password"
-        onChange={inputConfirmPassword}
-      />
-      <PrimaryButton
-        label="登録"
-        onClick={() => {
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
           if (validateForm(userName, email, password, confirmPassword)) {
             dispatch(signUp(userName, email, password));
           }
         }}
-      />
+      >
+        <TextInput
+          fullWidth
+          label="ユーザ名"
+          multiline={false}
+          required
+          rows={1}
+          value={userName}
+          type="text"
+          onChange={inputUserName}
+        />
+        <TextInput
+          fullWidth
+          label="メールアドレス"
+          multiline={false}
+          required
+          rows={1}
+          value={email}
+          type="email"
+          onChange={inputEmail}
+        />
+        <TextInput
+          fullWidth
+          label="パスワード（半角英数字6文字以上）"
+          minLength={6}
+          multiline={false}
+          required
+          rows={1}
+          value={password}
+          type="password"
+          onChange={inputPassword}
+        />
+        <TextInput
+          fullWidth
+          label="パスワードの再確認"
+          minLength={6}
+          multiline={false}
+          required
+          rows={1}
+          value={confirmPassword}
+          type="password"
+          onChange={inputConfirmPassword}
+        />
+        <PrimaryButton label="登録" type="submit" />
+      </form>
       <TextLink
         label="アカウントをお持ちの方はこちら"
         onClick={() => {
