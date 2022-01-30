e5558a169f09d0be8d97f03be55aa37781513fc3
Yuki-TU <k.yuki.90219@icloud.com>
2022-01-30 18:15:07 +0900

    add:#35 商品リポジトリの実装

---
+++ src/repository/product/firestore.ts
@@ -0,0 +1,53 @@
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { ProductsForDatabase } from '../../reducks/products/types';
+import { db } from '../../firebase';
+import type { IProductRepository } from './interface';
+
+/**
+ * firebaseの商品データ操作するためのリポジトリ
+ */
+class ProductFirebaseRepository implements IProductRepository {
+  private collection;
+
+  constructor() {
+    this.collection = 'products';
+  }
+
+  /**
+   * firebasestoreからproductIdの商品データ取得
+   * @param id 商品id
+   * @returns 商品データ
+   */
+  async fetch(id: string) {
+    try {
+      const referenceData = doc(db, this.collection, id);
+      const result = await getDoc(referenceData);
+      // 取得データを型変換して返却
+      return result.data() as ProductsForDatabase;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error(error.message);
+      }
+      throw new Error('failed to fetch firestore');
+    }
+  }
+
+  /**
+   * firebasestoreに商品情報を保存
+   * @param product 保存したい商品データ
+   */
+  async save(product: ProductsForDatabase) {
+    const key = product.id;
+
+    try {
+      // merge: trueで差分だけ保存ができる
+      await setDoc(doc(db, this.collection, key), product, { merge: true });
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error(error.message);
+      }
+    }
+  }
+}
+
+export default ProductFirebaseRepository;
