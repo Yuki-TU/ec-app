dd1b8588ccc5ba74975be19b439defd668c0bb87
Yuki-TU <k.yuki.90219@icloud.com>
2022-01-30 16:59:36 +0900

    add:#35 データベースに保存するユーザデータの型定義

--- src/reducks/users/types.ts
+++ src/reducks/users/types.ts
@@ -1,4 +1,6 @@
-/** ユーザー情報の型 */
+import { Timestamp } from 'firebase/firestore';
+
+/** reduxで扱うユーザー情報の型 */
 export type User = {
   customer_id: string;
   email: string;
@@ -6,5 +8,17 @@ export type User = {
   role: string;
   payment_method_id: string;
   uid: string;
+  username: string;
+};
+
+/** データベースで保存するユーザ情報の型 */
+export type UserForDatabase = {
+  customer_id: string;
+  created_at: Timestamp;
+  email: string;
+  role: string;
+  payment_method_id: string;
+  uid: string;
+  updated_at: Timestamp;
   username: string;
 };
