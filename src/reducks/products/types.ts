57b80dfd5ae4b65e50aa657f9deea91e1bfe0521
Yuki-TU <k.yuki.90219@icloud.com>
2022-01-30 18:29:25 +0900

    update:#35 データベースに保存する商品情報の型定義を修正 名前の修正 プロパティの修正

--- src/reducks/products/types.ts
+++ src/reducks/products/types.ts
@@ -1,7 +1,7 @@
 import { Timestamp } from 'firebase/firestore';
 
 /** データベースに保存する商品情報の型 */
-export type ProductsForDatabase = {
+export type ProductForDatabase = {
   /** 商品面 */
   name: string;
   /** 商品説明 */
@@ -11,11 +11,11 @@ export type ProductsForDatabase = {
   /** 性別 */
   gender: string;
   /** 値段 */
-  price: string;
+  price: number;
   /** 商品id */
   id: string;
   /** 商品画像 */
   images: { id: string; path: string }[];
   /** 更新日 */
-  update_at: Timestamp;
+  updated_at: Timestamp;
 };
--- src/repository/product/interface.ts
+++ src/repository/product/interface.ts
@@ -1,7 +1,7 @@
-import { ProductsForDatabase } from '../../reducks/products/types';
+import { ProductForDatabase } from '../../reducks/products/types';
 
 /** 商品情報のデータストア操作インターフェース */
 export interface IProductRepository {
-  fetch(id: string): Promise<ProductsForDatabase>;
-  save(user: ProductsForDatabase): void;
+  fetch(id: string): Promise<ProductForDatabase>;
+  save(user: ProductForDatabase): void;
 }
