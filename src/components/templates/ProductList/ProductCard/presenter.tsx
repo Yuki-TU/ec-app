55745bc67aba83f3ebd2f2989633b9e5a6beb94b
Yuki-TU <k.yuki.90219@icloud.com>
2022-02-13 13:15:25 +0900

    add:#38 商品情報カードに編集、削除メニューの見た目を追加 * 3連縦アイコンをメニューとした * メニューを押すと、編集する、削除するが表示

--- src/components/templates/ProductList/ProductCard/presenter.tsx
+++ src/components/templates/ProductList/ProductCard/presenter.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import { IconButton, Menu, MenuItem } from '@material-ui/core';
+import { MoreVert } from '@material-ui/icons';
+import { push } from 'connected-react-router';
+
 import type { ProductForDatabase } from '../../../../reducks/products/types';
 import noImage from '../../../../assets/images/no_image.png';
 import { useStyles } from './style';
+import { deleteProduct } from '../../../../reducks/products/operations';
 /**
  * 商品情報一覧で利用するカードのコンポーネント
  * @param props DB保存の商品情報
@@ -13,7 +19,25 @@ import { useStyles } from './style';
  */
 function ProductCard(props: ProductForDatabase) {
   const classes = useStyles();
-  const { images, name, price } = props;
+  const dispatch = useDispatch();
+  const { images, name, price, id } = props;
+
+  /** メニュー表示フラグ */
+  const [isOpeningMenu, setIsOpenMenu] = useState(false);
+  /** メニューを表示する位置設定 */
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleClickMenu = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setAnchorEl(event.currentTarget);
+    setIsOpenMenu(true);
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+    setIsOpenMenu(false);
+  };
 
   // 3桁ごとにコンマをつけstring型にする
   // 例) 2000000 -> '2,000,000'
@@ -33,6 +57,35 @@ function ProductCard(props: ProductForDatabase) {
             ¥{reshapePrime}
           </Typography>
         </div>
+        <IconButton
+          className={classes.icon}
+          onClick={(e) => handleClickMenu(e)}
+        >
+          <MoreVert />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl} // 表示位置の設定
+          keepMounted
+          open={isOpeningMenu}
+          onClose={handleCloseMenu}
+        >
+          <MenuItem
+            onClick={() => {
+              dispatch(push(`/product/edit/${id}`));
+              handleCloseMenu();
+            }}
+          >
+            編集する
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              dispatch(deleteProduct(id));
+              handleCloseMenu();
+            }}
+          >
+            削除する
+          </MenuItem>
+        </Menu>
       </CardContent>
     </Card>
   );
