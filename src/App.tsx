6c13f0ab472bb748b1e8e13c9b6c084fee046a44
Yuki-TU <k.yuki.90219@icloud.com>
2022-02-18 00:03:17 +0900

    remove:#41 index.htmlにスタイルを移動したため、App.tsxのスタイルは消去

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/styles';
 import Router from './Router';
-
-/** スタイル */
-const useStyles = makeStyles({
-  root: {
-    fontSize: '62.5%',
-  },
-});
 
 /**
  * ページ全体の関数コンポーネント
  * @return ページ全体のコンポーネント
  */
 function App() {
-  const classes = useStyles();
   return (
-    <div className={classes.root}>
+    <div>
       <main>
         <Router />
       </main>
