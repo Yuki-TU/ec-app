0839b451af755246986dad0c32b9b65f61885e34
Yuki-TU <k.yuki.90219@icloud.com>
2022-02-28 01:43:40 +0900

    add:#43 商品情報編集ページ内のalertをモーダルに変更 確認モーダル、エラーモーダル

--- src/components/templates/ProductEdit/ImageAddArea/presenter.tsx
+++ src/components/templates/ProductEdit/ImageAddArea/presenter.tsx
@@ -1,9 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import AddPhotoAlternateIcon from '@material-ui/icons/AddPhotoAlternate';
 import { useStyles } from './style';
 import { deleteImage, uploadImage } from './hook';
 import { ImagePreview } from '../../../uiParts/ImagePreview';
+import { Dialog } from '../../../uiParts/Dialog';
 
 /** 引数の型定義 */
 type ImageAddAreaProps = {
@@ -21,23 +22,66 @@ type ImageAddAreaProps = {
 function ImageAddArea(props: ImageAddAreaProps) {
   const classes = useStyles();
   const { setImages, images } = props;
+  const [openUploadFailureDialog, setOpenUploadFailureDialog] = useState(false);
+  const [openDeleteFailureDialog, setOpenDeleteFailureDialog] = useState(false);
+  const [openConfirmDialog, setOpenConfirmDialog] = useState(false);
+  const [deletedImageId, setDletedImageId] = useState('');
+
+  // 画像アップロード処理
+  const hadleUploadImage = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    try {
+      await uploadImage(event, images, setImages);
+    } catch (error) {
+      setOpenUploadFailureDialog(true);
+    }
+  };
+  // 画像の削除処理
+  const handdleDeleteImage = async (imageId: string) => {
+    try {
+      await deleteImage(imageId, images, setImages);
+    } catch (error) {
+      setOpenDeleteFailureDialog(true);
+    }
+  };
 
   return (
     <div className={classes.root}>
+      <Dialog
+        isOpen={openUploadFailureDialog}
+        setIsOpen={setOpenUploadFailureDialog}
+        title="⚠エラー"
+        text="画像のアップロードに失敗しました。もう一度やり直してください。"
+      />
+      <Dialog
+        isOpen={openDeleteFailureDialog}
+        setIsOpen={setOpenDeleteFailureDialog}
+        title="⚠エラー"
+        text="画像の削除に失敗しました。もう一度やり直してください。"
+      />
       <div className={classes.images}>
         {images.length > 0 &&
           images.map((image) => (
-            <ImagePreview
-              alt="プレビュー画像"
-              id={image.id}
-              key={image.id}
-              path={image.path}
-              onClick={() => {
-                if (window.confirm('この画像を削除しますか？')) {
-                  deleteImage(image.id, images, setImages);
-                }
-              }}
-            />
+            <>
+              <Dialog
+                isOpen={openConfirmDialog}
+                setIsOpen={setOpenConfirmDialog}
+                title="確認"
+                text="本当にこの画像を削除しますか？"
+                onClick={() => handdleDeleteImage(deletedImageId)}
+              />
+              <ImagePreview
+                alt="プレビュー画像"
+                id={image.id}
+                key={image.id}
+                path={image.path}
+                onClick={() => {
+                  setDletedImageId(image.id);
+                  setOpenConfirmDialog(true);
+                }}
+              />
+            </>
           ))}
       </div>
       <IconButton>
@@ -50,7 +94,7 @@ function ImageAddArea(props: ImageAddAreaProps) {
             type="file"
             id="add-image"
             onChange={useCallback(
-              (event) => uploadImage(event, images, setImages),
+              (event) => hadleUploadImage(event),
               [setImages]
             )}
           />
