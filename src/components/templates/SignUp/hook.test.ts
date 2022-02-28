4f2e8d0b858163cee5667a725e0eaea7b13f2d1d
Yuki-TU <k.yuki.90219@icloud.com>
2022-02-28 22:58:17 +0900

    add:#43 signup画面のエラーをモーダル化

--- src/components/templates/SignUp/hook.test.ts
+++ src/components/templates/SignUp/hook.test.ts
@@ -1,10 +1,6 @@
 import { validatePassword } from './hook';
 
 describe('validPasswordはパスワードと確認パスワードが一致するか判断', () => {
-  beforeAll(() => {
-    // window.alertをモックにする。Jestではモックにしないとエラーになる
-    window.alert = jest.fn();
-  });
   describe('passwordと確認passwordが異なっている場合はfalse', () => {
     test(`passworと確認passworがtestpasswordとtesttestの場合はfalse`, () => {
       expect(validatePassword('testpassword', 'testtest')).toBe(false);
