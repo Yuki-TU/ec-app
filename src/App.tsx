034c2d622ea11337c425d9042b141335f2afbf91
Yuki-TU <k.yuki.90219@icloud.com>
2022-03-05 02:40:17 +0900

    add:#45 ボディ部分尾パディング設定とヘッダー部分を指定

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
+import { makeStyles } from '@material-ui/styles';
 import React from 'react';
+import { Header } from './components/uniqueParts/Header';
 import Router from './Router';
 
+const useStyles = makeStyles({
+  main: {
+    padding: '70px 0',
+  },
+});
 /**
  * ページ全体の関数コンポーネント
  * @return ページ全体のコンポーネント
  */
 function App() {
+  const classes = useStyles();
   return (
-    <div>
-      <main>
+    <>
+      <header>
+        <Header />
+      </header>
+      <main className={classes.main}>
         <Router />
       </main>
-    </div>
+    </>
   );
 }
 
