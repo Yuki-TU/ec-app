f5aaec8d2720030483695c0e6f30b062150c66d6
Yuki-TU <k.yuki.90219@icloud.com>
2022-03-05 02:38:26 +0900

    add:#45 ヘッダーメニューコンポーネント ログイン状態を確認し、メニューを切り替えてている

---
+++ src/components/uniqueParts/Header/style.ts
@@ -0,0 +1,22 @@
+import { makeStyles } from '@material-ui/styles';
+
+/** スタイル */
+export const useStyles = makeStyles({
+  menuBar: {
+    backgroundColor: '#fff',
+    color: '#444',
+  },
+  toolbar: {
+    padding: 0,
+    margin: '0 auto',
+    maxWidth: 1024,
+    width: '100%',
+  },
+  menus: {
+    margin: '0 0 0 auto',
+  },
+  siteLogo: {
+    width: '66.8px',
+    height: '66.8px',
+  },
+});
