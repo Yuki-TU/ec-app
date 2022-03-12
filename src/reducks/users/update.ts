88181062b855cee8970834f14ac1289f67f588aa
Yuki-TU <k.yuki.90219@icloud.com>
2022-03-13 00:49:21 +0900

    add:#47 お気に入り商品情報を更新するスライスを定義

--- src/reducks/users/update.ts
+++ src/reducks/users/update.ts
@@ -11,7 +11,7 @@ const userSlice = createSlice({
   reducers: {
     signInAction: (state: User, action: PayloadAction<User>) => {
       // 前回値に対して、データ変更箇所だけを更新しupdateDataに代入
-      const updatedData = { ...initialUserState, ...action.payload };
+      const updatedData = { ...state, ...action.payload };
       // ストアの値の更新
       return updatedData;
     },
@@ -20,11 +20,25 @@ const userSlice = createSlice({
       const updatedData = { ...initialUserState };
       return updatedData;
     },
+    /**
+     * お気に入り商品リストスライス
+     * @param state 現在のUserステート
+     * @param action 更新するお気に入り商品リスト
+     * @returns 更新後のUser
+     */
+    updateFavoriteProductAction: (
+      state: User,
+      action: PayloadAction<string[]>
+    ) => {
+      const updatedData = { ...state, favoriteProducts: [...action.payload] };
+      return updatedData;
+    },
   },
 });
 
 // action creatorをエクスポート
-export const { signInAction, signOutAction } = userSlice.actions;
+export const { signInAction, signOutAction, updateFavoriteProductAction } =
+  userSlice.actions;
 
 // reducerをエクスポート
 export const user = userSlice.reducer;
