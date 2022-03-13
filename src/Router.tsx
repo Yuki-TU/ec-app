0ac9c49a838c77dbc7c4d69c6b4d4ee536c55a08
Yuki-TU <k.yuki.90219@icloud.com>
2022-03-13 17:47:06 +0900

    update:#47 ルーティング処理 アカウント一覧のルーティング お気に入り一覧のルーティング

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,8 +6,9 @@ import { SignIn } from './components/templates/SignIn';
 import { Account } from './components/templates/Account';
 import Auth from './Auth';
 import { ProductEdit } from './components/templates/ProductEdit';
-import { ProductList } from './components/templates/ProductList';
+import { AllProductList } from './components/templates/AllProductList';
 import { ProductDetail } from './components/templates/ProductDetail';
+import { FavoriteProductList } from './components/templates/FavoriteProductList';
 
 /**
  * メインボディのルーティングを実施、
@@ -22,9 +23,14 @@ function Router() {
       <Route exact path="/signin/reset" component={PasswordReset} />
       <Auth>
         <>
-          <Route exact path="(/)?" component={ProductList} />
+          <Route exact path="(/)?" component={AllProductList} />
           <Route exact path="/product/:id" component={ProductDetail} />
           <Route exact path="/account" component={Account} />
+          <Route
+            exact
+            path="/favorite-products"
+            component={FavoriteProductList}
+          />
           <Route path="/product/edit(/:id)?" component={ProductEdit} />
         </>
       </Auth>
