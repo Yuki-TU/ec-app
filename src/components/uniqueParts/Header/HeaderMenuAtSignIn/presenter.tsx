e69ab9bb9104885477cad1472046a9def3742965
Yuki-TU <k.yuki.90219@icloud.com>
2022-03-13 17:44:26 +0900

    add:#47 サインイン時のヘッダーにメニュー追加

--- src/components/uniqueParts/Header/HeaderMenuAtSignIn/presenter.tsx
+++ src/components/uniqueParts/Header/HeaderMenuAtSignIn/presenter.tsx
@@ -2,10 +2,11 @@ import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import PersonIcon from '@material-ui/icons/Person';
 import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
 import { push } from 'connected-react-router';
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { signOut } from '../../../../reducks/users/operations';
 import { IconButton } from '../../../uiParts/IconButton';
+import { Menu } from '../../../uiParts/Menu';
 
 /**
  * ログイン時のヘッダーメニューコンポーネント
@@ -13,13 +14,43 @@ import { IconButton } from '../../../uiParts/IconButton';
  */
 function HeaderMenuAtSignIn() {
   const dispatch = useDispatch();
+  // メニュー開閉を担うステート
+  const [openAccountMenu, setOpenAccountMenu] = useState(false);
+  // アカウントアイコンののエレメント、アカウントめニューの表示位置の指定に利用
+  const accountMenuAnchorRef = useRef<HTMLButtonElement>(null);
+
+  const toggleAccountMenu = useCallback(() => {
+    setOpenAccountMenu((prevOpenAccountMenu) => !prevOpenAccountMenu);
+  }, [setOpenAccountMenu]);
+
+  const accoutMenuItems = [
+    {
+      onClick: () => {
+        dispatch(push('/account'));
+      },
+      label: 'アカウント情報',
+    },
+    {
+      onClick: () => {
+        dispatch(push('/favorite-products'));
+      },
+      label: 'お気に入り一覧',
+    },
+  ];
 
   return (
     <>
       <IconButton
-        onClick={() => dispatch(push('/account'))}
+        reference={accountMenuAnchorRef}
+        onClick={toggleAccountMenu}
         icon={<PersonIcon />}
         label="アカウント"
+      />
+      <Menu
+        menuItems={accoutMenuItems}
+        reference={accountMenuAnchorRef}
+        openMenu={openAccountMenu}
+        setOpenMenu={setOpenAccountMenu}
       />
       <IconButton
         onClick={() => dispatch(push('/product/edit'))}
