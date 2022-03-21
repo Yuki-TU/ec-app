7d72d507558a6e202013f83efcf6c2d6185cf245
Yuki-TU <k.yuki.90219@icloud.com>
2022-03-21 16:14:50 +0900

    add:#51 非同期処理を中断する処理とダイアログを表示する処理を追加

--- src/components/pages/FavoriteProductList/presenter.tsx
+++ src/components/pages/FavoriteProductList/presenter.tsx
@@ -3,6 +3,7 @@ import { ProductForDatabase } from '../../../reducks/products/types';
 import { useSelector } from '../../../reducks/store';
 import { loadFavoriteProducts } from '../../../reducks/users/selectors';
 import { ProductFirebaseRepository } from '../../../repository/product';
+import { Dialog } from '../../uniqueParts/Dialog';
 import { ProductList } from '../../uniqueParts/ProductList';
 import { useStyles } from './style';
 
@@ -17,23 +18,41 @@ function FavoriteProductList() {
   const selector = useSelector((state) => state);
   const favoriteProductIds = loadFavoriteProducts(selector);
 
+  const [openFailureDialog, setOpenFailureDialog] = useState(false);
   const [favoriteProducts, setFavoriteProducts] = useState<
     ProductForDatabase[]
   >([]);
 
   // コンポーネントを表示したら、商品データを取得する
   useEffect(() => {
-    (async () => {
-      const repository = new ProductFirebaseRepository();
-      const products = await repository.findByIds(favoriteProductIds);
-      setFavoriteProducts(products);
-    })();
+    // 非同期処理を中断するためのクラス
+    const abortCtrl = new AbortController();
+
+    try {
+      (async () => {
+        const repository = new ProductFirebaseRepository();
+        const products = await repository.findByIds(favoriteProductIds);
+        setFavoriteProducts(products);
+      })();
+    } catch (error) {
+      setOpenFailureDialog(true);
+    }
+    return () => {
+      // 非同期処理を完了する前に、コンポーネントがアンマウントされたら非同期処理を中断する
+      abortCtrl.abort();
+    };
   }, []);
 
   return (
     <section className={classes.root}>
+      <Dialog
+        isOpen={openFailureDialog}
+        setIsOpen={setOpenFailureDialog}
+        title="⚠エラー"
+        text="不具合が発生しました。通信状況を確認しリロードし直してください。"
+      />
       <h1 className={classes.headerTitle}>お気に入り商品</h1>
-      {favoriteProductIds.length ? (
+      {favoriteProducts.length ? (
         <ProductList list={favoriteProducts} />
       ) : (
         <p className={classes.noFavoriteText}>お気に入りした商品がありません</p>
