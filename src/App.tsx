36627047063bff7a0adcc588cf6e68df91c16c9b
Yuki-TU <k.yuki.90219@icloud.com>
2022-05-08 17:48:21 +0900

    update:#57 tailwindに書き換え

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import { makeStyles } from '@material-ui/styles';
 import React from 'react';
 import { Header } from './components/uniqueParts/Header';
 import Router from './Router';
 
-const useStyles = makeStyles({
-  main: {
-    padding: '70px 0',
-  },
-});
 /**
  * ページ全体の関数コンポーネント
  * @return ページ全体のコンポーネント
  */
 function App() {
-  const classes = useStyles();
   return (
     <>
       <header>
         <Header />
       </header>
-      <main className={classes.main}>
+      <main className="py-[70px]">
         <Router />
       </main>
     </>
