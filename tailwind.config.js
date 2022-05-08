fa3f7d7b0d3b80b688fe3ec67c7dc5a7432ef53b
Yuki-TU <k.yuki.90219@icloud.com>
2022-05-08 17:18:13 +0900

    add:#57 tailwindcss, postcssの設定ファイル作成

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    // class属性を含む全てのファイルを指定する必要がある(jsx,htmlなど)
+    './public/**/*.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+    // フォントの設定
+    // font-originでアクセス`
+    fontFamily: {
+      origin: [
+        'Helvetica Neue',
+        'Arial',
+        'Hiragino Kaku Gothic ProN',
+        'Hiragi',
+        'Meiryo',
+        'sans-serif',
+      ],
+    },
+  },
+  plugins: [],
+};
