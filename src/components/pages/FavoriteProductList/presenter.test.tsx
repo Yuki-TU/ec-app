3cdce7892fcd663645ee21e09de1c06de29b22a8
Yuki-TU <k.yuki.90219@icloud.com>
2022-05-10 02:12:40 +0900

    add:#59 お気に入りページのスタイル移行、ローディングの追加

--- src/components/pages/FavoriteProductList/presenter.test.tsx
+++ src/components/pages/FavoriteProductList/presenter.test.tsx
@@ -1,26 +1,26 @@
 import '@testing-library/jest-dom';
 import { render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
+import { useQuery } from 'react-query';
 import { FavoriteProductList } from '.';
 import { useSelector } from '../../../reducks/store';
 import { loadFavoriteProducts } from '../../../reducks/users/selectors';
 import { ProductFirebaseRepository } from '../../../repository/product';
-import { useStyles } from './style';
 
 // モジュールのモック化
 jest.mock('connected-react-router', () => jest.fn());
 jest.mock('react-redux');
-jest.mock('./style');
+jest.mock('react-query');
 jest.mock('../../../reducks/users/selectors');
 jest.mock('../../../reducks/store');
 jest.mock('../../../repository/product');
 jest.mock('../../../firebase', () => {});
 
 // const mockUseDispatch = useDispatch as jest.Mock;
-const mockUseStyles = useStyles as jest.Mock;
 const mockUseSelector = useSelector as jest.Mock;
 const mockLoadFavoriteProducts = loadFavoriteProducts as unknown as jest.Mock;
 const mockProductFirebaseRepository = ProductFirebaseRepository as jest.Mock;
+const mockUseQuery = useQuery as jest.Mock;
 
 beforeEach(() => {
   mockProductFirebaseRepository.mockClear();
@@ -29,7 +29,7 @@ beforeEach(() => {
 describe('FavoriteProductListコンポーネントはお気に入り商品一覧を表示する', () => {
   test('お気に入り商品がない場合は「お気に入りした商品がありません」と表示する', async () => {
     // モックの帰り値を指定
-    mockUseStyles.mockReturnValue({});
+    mockUseQuery.mockReturnValue({});
     mockUseSelector.mockReturnValue({});
     mockLoadFavoriteProducts.mockReturnValue([]);
     mockProductFirebaseRepository.mockImplementationOnce(() => ({
@@ -60,9 +60,9 @@ describe('FavoriteProductListコンポーネントはお気に入り商品一覧
   test.skip('お気に入り商品がある場合は商品リスト一覧を表示する', () => {});
   test('ヘッダーテキストに「お気に入り商品」と表示する', async () => {
     // モックの帰り値を指定
-    mockUseStyles.mockReturnValue({});
     mockUseSelector.mockReturnValue({});
     mockLoadFavoriteProducts.mockReturnValue([]);
+    mockUseQuery.mockReturnValue({});
     mockProductFirebaseRepository.mockImplementationOnce(() => ({
       findByIds: () => ({
         name: '',
