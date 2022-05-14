627d6dae187904ff191d9a59fc47d98c4be9c0df
Yuki-TU <k.yuki.90219@icloud.com>
2022-05-14 18:37:16 +0900

    update:#66 テストのstyleのモックを削除

--- src/components/pages/ProductEdit/presenter.test.tsx
+++ src/components/pages/ProductEdit/presenter.test.tsx
@@ -4,20 +4,17 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { ProductEdit } from '.';
 import { useStyles } from './style';
-import { useStyles as buttonUseStyle } from '../../uiParts/PrimaryButton/style';
 import { loadUserId } from '../../../reducks/users/selectors';
 
 // モジュールのモック化
 jest.mock('connected-react-router', () => jest.fn());
 jest.mock('react-redux');
 jest.mock('./style');
-jest.mock('../../uiParts/PrimaryButton/style');
 jest.mock('../../../firebase', () => {});
 jest.mock('../../../reducks/users/selectors');
 
 const mockUseDispatch = useDispatch as jest.Mock;
 const mockUseStyles = useStyles as jest.Mock;
-const mockButtonStyle = buttonUseStyle as jest.Mock;
 const mockLoadUserId = loadUserId as unknown as jest.Mock;
 
 describe('ProductEditコンポーネントは、商品情報を登録する画面', () => {
@@ -25,7 +22,6 @@ describe('ProductEditコンポーネントは、商品情報を登録する画�
     // モックの返り値を指定
     mockUseStyles.mockReturnValue({});
     mockUseDispatch.mockReturnValue({});
-    mockButtonStyle.mockReturnValue({});
     mockLoadUserId.mockReturnValue('1111');
 
     render(<ProductEdit />);
