f22cab78b260b1b722e155010d38d57b89600895
Yuki-TU <k.yuki.90219@icloud.com>
2022-05-14 18:34:52 +0900

    update:#66 PrimaryButtonを修正

--- src/components/uiParts/PrimaryButton/presenter.test.tsx
+++ src/components/uiParts/PrimaryButton/presenter.test.tsx
@@ -2,16 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { PrimaryButton } from '.';
-import { useStyles } from './style';
-
-// スタイル指定のuseStyles関数はメソッド化
-jest.mock('./style');
-const mockUseStyles = useStyles as jest.Mock;
 
 describe('PrimeButtonンポーネントは汎用ボタンを表示', () => {
   beforeEach(() => {
-    // useStyleの返り値を空のオブジェクトに設定
-    mockUseStyles.mockReturnValue({});
     render(<PrimaryButton label="ok" type="button" />);
   });
   test('ボタンタグを表示している', () => {
